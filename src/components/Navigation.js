7bafa3a24e454895719ee08fd51fc42a8a0b63a5
James Reilly <jreilly2112@gmail.com>
2022-05-27 21:39:17 -0400

    updating file scheme

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,12 +3,29 @@ import React from 'react';
 export default function Navigation({ currentPage, handlePageChange }) {
     
     return (
-    <nav>
-        <a href="#Home" onClick={() => handlePageChange('Home')}>Home</a>
-        <a href="#AboutMe" onClick={() => handlePageChange('About')}>About Me</a>
-        {/* <a href="#MySkills">My Skills</a> */}
-        <a href="#MyPortfolio" onClick={() => handlePageChange('Projects')}>Portfolio</a>
-        <a href="#ContactMe" onClick={() => handlePageChange('Contact')}>Contact Me</a>
+        <nav class="flex">
+
+        <a id="about-me-nav" href="#about-me">About Me</a>
+
+        <div class="dropdown">
+            <button class="dropdown-button">Content</button>
+            <ul class="dropdown-content">
+                <li>
+                    <a href="#portfolio" onclick="clickimage(document.getElementById('bot'), document.getElementById('bot-cap'), 'www.github.com/WanderingLad/Discord-Bot')">Discord Bot</a>
+                </li>
+                <li>
+                    <a href="#portfolio" onclick="clickimage(document.getElementById('code'), document.getElementById('code-cap'), 'www.github.com/WanderingLad/Jimmy-Reilly-Portfolio')">My Portfolio Code</a>
+                </li>
+                <li>
+                    <a href="#portfolio" onclick="clickimage(document.getElementById('stamina'), document.getElementById('stamina-cap'), 'www.projectstamina.com/')">Project Stamina</a>
+                </li>
+                <li>
+                    <a href="#portfolio" onclick="clickimage(document.getElementById('seoul'), document.getElementById('seoul-cap'), 'www.fourseasons.com')">Seoul</a>
+                </li>
+            </ul>
+        </div>
+
+        <a id="contact-me-nav" href="#contact-me">Contact Me</a>
     </nav>
     )
 }
